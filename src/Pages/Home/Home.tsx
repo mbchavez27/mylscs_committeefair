cda18694630fcc463a8f5d5c6115912b167ffb70
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-01-16 19:49:23 +0800

    add camera

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,12 +1,32 @@
 import "./Home.css";
+import BarcodeScanner from "../../Components/Barcode.tsx"
+import axios, { AxiosError } from "axios";
+import { useEffect, useState } from "react";
+
 
 export const Home = () => {
-  const userProfile = {
-    name: "Sean Denzel Robenta",
-    position: "Vice President",
-    committee: "Research and Development",
-    email: "sean_robenta@dlsu.edu.ph",
-  };
+  const [id, setID] = useState(null)<number>;
+  const [data, setData] = useState(null)<any>;
+
+
+ useEffect(() => {
+    const fetchData = async () => {
+        const response = await axios.post("http://jogacf.app.dlsu-lscs.org/?id=" + id, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      setData(data);
+    };
+    fetchData();
+  }, [id]);
+
+
+
+   const onNewScanResult = (decodedText, decodedResult) => {
+    setID(decodedText);
+    console.log(decodedText);
+    };
 
   return (
     <>
@@ -16,29 +36,35 @@ export const Home = () => {
         <div>
           {/* Header*/}
           <div className="header">
-            <div className="headerTitle">Hello, {userProfile.name}</div>
-            <div className="headerDesc">
-              <span className="position">{userProfile.position}</span>{" "}
-              {userProfile.committee}
-            </div>
-            <div className="headerDesc">{userProfile.email}</div>
-          </div>
-          {/*   Fun Fact       */}
-          <div className="funFact">
-            <div className="factTitle">Did you Know?</div>
-            <div className="factDesc">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam
-              rem recusandae et minima natus? Sunt quod voluptas amet error
-              vero! Quo nostrum vel dicta deleniti hic recusandae atque eaque
-              optio.
-            </div>
-          </div>
+            {data == null ? 
+<p>Scan the ID</p>
+            : (<>
+          <div className="headerTitle">Hello, {data.lscs_data.full_name||"Scan the barcode behind your id!"}</div>
+          <div className="headerDesc">
+            <span className="position">{data.lscs_data.position_name || " "}</span>{" "}
+          {data.lscs_data.committee_name || " "}</div>
+          <div className="headerDesc">{data.lscs_data.email || " "}</div>
+              </>
+            )}
+          {/* {/*   Fun Fact       */} 
+          {/* <div className="funFact"> */}
+          {/*   <div className="factTitle">Did you Know?</div> */}
+          {/*   <div className="factDesc"> */}
+          {/*     {data.llm_data.candidates.content.parts[0].text || " "} */}
+          {/*   </div> */}
+          {/* </div> */}
         </div>
         {/*   Scan ID        */}
         <div>
           <div className="scanTitle">Scan the back of your ID here!</div>
-          <div className="scanID"></div>
+           <BarcodeScanner
+                fps={10}
+                qrbox={250}
+                disableFlip={true}
+                qrCodeSuccessCallback={onNewScanResult}
+            />
         </div>
+      </div>
       </div>
     </>
   );

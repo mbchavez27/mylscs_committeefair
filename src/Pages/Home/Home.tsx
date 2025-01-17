c91a5731f2234a5932a94cdbf76fec3a62905ebf
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-01-18 04:15:48 +0800

    add timeout to reset id

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,34 +1,35 @@
 import "./Home.css";
-import BarcodeScanner from "../../Components/Barcode.tsx"
+import BarcodeScanner from "../../Components/Barcode.tsx";
 import axios, { AxiosError } from "axios";
 import { useEffect, useState } from "react";
 
-
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 export const Home = () => {
   const [id, setID] = useState(12328146)<number>;
   const [data, setData] = useState(null)<any>;
 
-
- useEffect(() => {
+  useEffect(() => {
     const fetchData = async () => {
-        const response = await axios.post("http://jogacf.app.dlsu-lscs.org/?id=" + id, {
+      const response = await axios.post(
+        "http://jogacf.app.dlsu-lscs.org/?id=" + id,
+        {
           headers: {
             "Content-Type": "application/json",
           },
-        });
+        }
+      );
       setData(response.data);
       console.log(response.data);
+      await sleep(10000);
+      setData(null);
     };
     fetchData();
   }, [id]);
 
-
-
-   const onNewScanResult = (decodedText, decodedResult) => {
+  const onNewScanResult = (decodedText, decodedResult) => {
     setID(decodedText);
     console.log(decodedResult);
-    };
-
+  };
 
   return (
     <>
@@ -38,36 +39,38 @@ export const Home = () => {
         <div>
           {/* Header*/}
           <div className="header">
-            <div className="headerTitle">Hello, {" "} 
-            {data == null ? null : <>{data.lscs_data.full_name}</>}!
+            <div className="headerTitle">
+              Hello, {data == null ? null : <>{data.lscs_data.full_name}</>}!
             </div>
             <div className="headerDesc">
               <span className="position">
-            {data == null ? null : <>{data.lscs_data.position_name}, </>}
+                {data == null ? null : <>{data.lscs_data.position_name}, </>}
               </span>{" "}
-            {data == null ? null : <>{data.lscs_data.committee_name}</>}
+              {data == null ? null : <>{data.lscs_data.committee_name}</>}
             </div>
             <div className="headerDesc">
-            {data == null ? null : <>{data.lscs_data.email}</>}
+              {data == null ? null : <>{data.lscs_data.email}</>}
             </div>
           </div>
           {/*   Fun Fact       */}
           <div className="funFact">
             <div className="factTitle">Did you Know?</div>
             <div className="factDesc">
-            {data == null ? null : <>{data.llm_data.candidates[0].content.parts[0].text}</>}
+              {data == null ? null : (
+                <>{data.llm_data.candidates[0].content.parts[0].text}</>
+              )}
             </div>
           </div>
         </div>
         {/*   Scan ID        */}
         <div>
           <div className="scanTitle">Scan the back of your ID here!</div>
-           <BarcodeScanner
-                fps={30}
-                qrbox={240}
-                disableFlip={false}
-                qrCodeSuccessCallback={onNewScanResult}
-            />
+          <BarcodeScanner
+            fps={30}
+            qrbox={240}
+            disableFlip={false}
+            qrCodeSuccessCallback={onNewScanResult}
+          />
         </div>
       </div>
     </>
